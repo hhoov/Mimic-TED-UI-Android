201b187f531a8b8eb17fd68be75eed7f250d7a7e
Hannah Hoover <hannahghoover@gmail.com>
2018-07-25 16:40:13 -0500

    Added RecyclerView to Podcasts and Playlists tabs. Still working on the CardView for them.

--- app/src/main/java/com/example/hannahhoover/mimic_ted_ui_android/PlaylistsTabFragment.java
+++ app/src/main/java/com/example/hannahhoover/mimic_ted_ui_android/PlaylistsTabFragment.java
@@ -2,7 +2,12 @@ package com.example.hannahhoover.mimic_ted_ui_android;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +18,6 @@ import android.view.ViewGroup;
  */
 public class PlaylistsTabFragment extends Fragment {
 
-
     public PlaylistsTabFragment() {
         // Required empty public constructor
     }
@@ -23,7 +27,15 @@ public class PlaylistsTabFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tab_playlists_fragment, container, false);
+        View view = inflater.inflate(R.layout.tab_playlists_fragment, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this.getActivity(), 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter();
+        recyclerView.setAdapter(adapter);
+        return recyclerView;
     }
 
 }
--- app/src/main/java/com/example/hannahhoover/mimic_ted_ui_android/PodcastsTabFragment.java
+++ app/src/main/java/com/example/hannahhoover/mimic_ted_ui_android/PodcastsTabFragment.java
@@ -3,6 +3,8 @@ package com.example.hannahhoover.mimic_ted_ui_android;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +15,6 @@ import android.view.ViewGroup;
  */
 public class PodcastsTabFragment extends Fragment {
 
-
     public PodcastsTabFragment() {
         // Required empty public constructor
     }
@@ -23,7 +24,15 @@ public class PodcastsTabFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tab_podcasts_fragment, container, false);
+        View view = inflater.inflate(R.layout.tab_podcasts_fragment, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this.getActivity(), 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter();
+        recyclerView.setAdapter(adapter);
+        return view;
     }
 
 }
