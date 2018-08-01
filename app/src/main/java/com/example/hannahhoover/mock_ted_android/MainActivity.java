1d9ea82bc99f4e708827632390d4ffdeb081eb89
Hannah Hoover <hannahghoover@gmail.com>
2018-08-01 18:28:59 -0500

    Set up a cleaner way (I think) of adding fragments and tabs, as well as the icons for the tabs. I took out the addOnPageChangeListener and addOnTabSelectedListener to clean up, as I'm not working with that just yet.

--- app/src/main/java/com/example/hannahhoover/mock_ted_android/MainActivity.java
+++ app/src/main/java/com/example/hannahhoover/mock_ted_android/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.hannahhoover.mock_ted_android;
 
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private int[] tabIcons = {R.drawable.ic_tab_talks,
+                            R.drawable.ic_tab_playlists,
+                            R.drawable.ic_tab_podcasts,
+                            R.drawable.ic_tab_surpriseme_temp,
+                            R.drawable.ic_tab_mytalks};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,55 +24,35 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final ViewPager viewPager = findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
         // Create an instance of the tab layout from the view
-        TabLayout tabLayout = findViewById(R.id.tab_layout);
-        // Set the text for each tab
-        tabLayout.addTab(tabLayout.newTab()
-                .setIcon(R.drawable.ic_tab_talks)
-                .setContentDescription(R.string.talks));
-        tabLayout.addTab(tabLayout.newTab()
-                .setIcon(R.drawable.ic_tab_playlists)
-                .setContentDescription(R.string.playlists));
-        tabLayout.addTab(tabLayout.newTab()
-                .setIcon(R.drawable.ic_tab_podcasts)
-                .setContentDescription(R.string.podcasts));
-        tabLayout.addTab(tabLayout.newTab()
-                .setIcon(R.drawable.ic_tab_surpriseme_temp)
-                .setContentDescription(R.string.surprise_me));
-        tabLayout.addTab(tabLayout.newTab()
-                .setIcon(R.drawable.ic_tab_mytalks)
-                .setContentDescription(R.string.my_talks));
-        // Set the tabs to fill the entire layout
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
 
-        // Use PagerAdapter to manage page views in each activity ?
-        // Each page is represented by its own activity
-        // This is another example of the adapter pattern
-        final ViewPager viewPager = findViewById(R.id.pager);
-        final PagerAdapter adapter = new CustomPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+    }
+
+    // Todo -- NullPointer warnings here - find if there's a safer way to set the icons than this
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+        tabLayout.getTabAt(3).setIcon(tabIcons[3]);
+        tabLayout.getTabAt(4).setIcon(tabIcons[4]);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        CustomPagerAdapter adapter = new CustomPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TalksTabFragment());
+        adapter.addFragment(new PlaylistsTabFragment());
+        adapter.addFragment(new PodcastsTabFragment());
+        adapter.addFragment(new SurpriseMeTabFragment());
+        adapter.addFragment(new MyTalksTabFragment());
         viewPager.setAdapter(adapter);
-
-        // Setting a listener for clicks
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-
-
     }
 
     @Override
