12de589663a370d8bf0bc814309428df4a9d76d5
Hannah Hoover <hannahghoover@gmail.com>
2018-08-01 18:32:22 -0500

    Along with the MainActivity, I cleaned this up somewhat by simply returning the fragment at a specific position in the fragment list to MainActivity's setupViewPager method instead of using a switch-case statement to add fragments.

--- app/src/main/java/com/example/hannahhoover/mock_ted_android/CustomPagerAdapter.java
+++ app/src/main/java/com/example/hannahhoover/mock_ted_android/CustomPagerAdapter.java
@@ -4,36 +4,28 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomPagerAdapter extends FragmentStatePagerAdapter {
 
-    private int numOfTabs;
+    private final List<Fragment> fragmentList = new ArrayList<>();
 
-    public CustomPagerAdapter(FragmentManager fragmentManager, int numOfTabs) {
+    CustomPagerAdapter(FragmentManager fragmentManager) {
         super(fragmentManager);
-        this.numOfTabs = numOfTabs;
     }
-
 
     @Override
     public Fragment getItem(int position) {
-        switch (position) {
-            case 0:
-                return new TalksTabFragment();
-            case 1:
-                return new PlaylistsTabFragment();
-            case 2:
-                return new PodcastsTabFragment();
-            case 3:
-                return new SurpriseMeTabFragment();
-            case 4:
-                return new MyTalksTabFragment();
-            default:
-                return null;
-        }
+        return fragmentList.get(position);
     }
 
     @Override
     public int getCount() {
-        return numOfTabs;
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        fragmentList.add(fragment);
     }
 }
